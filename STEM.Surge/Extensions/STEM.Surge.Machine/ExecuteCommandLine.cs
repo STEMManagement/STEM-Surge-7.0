71feabb77d06cd65508d91340b0e32afed13e676
STEM Management <brendan.b.richards@gmail.com>
2020-02-18 10:47:13 +0000

    Updated to handle timeout in addition to other improvements

--- STEM.Surge/Extensions/STEM.Surge.Machine/ExecuteCommandLine.cs
+++ STEM.Surge/Extensions/STEM.Surge.Machine/ExecuteCommandLine.cs
@@ -30,12 +30,28 @@ namespace STEM.Surge.Machine
     {
         [DisplayName("Executable Name"), DescriptionAttribute("What is the command line executable name (use the full path if necessary)?")]
         public string ProcessName { get; set; }
+        
         [DisplayName("Command Line Arguments"), DescriptionAttribute("What would be typed after the command line executable name if you were running this manually?")]
         public string Arguments { get; set; }
+        
         [DisplayName("Number of retries"), DescriptionAttribute("How many times should each operation be attempted?")]
         public int Retry { get; set; }
+        
         [DisplayName("Seconds between retries"), DescriptionAttribute("How many seconds should be waited between retries?")]
         public double RetryDelaySeconds { get; set; }
+        
+        [DisplayName("Application Timeout Seconds"), DescriptionAttribute("How many seconds before the process is aborted?")]
+        public int ApplicationTimeoutSeconds { get; set; }
+
+        public ExecuteCommandLine()
+        {
+            ProcessName = "dir";
+            Arguments = "";
+
+            Retry = 1;
+            RetryDelaySeconds = 3;
+            ApplicationTimeoutSeconds = 300;
+        }
 
         protected override bool _Run()
         {
@@ -46,10 +62,46 @@ namespace STEM.Surge.Machine
                 {
                     Process p = new Process();
                     p.StartInfo = new ProcessStartInfo(ProcessName, Arguments);
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.CreateNoWindow = true; 
+                    p.StartInfo.RedirectStandardInput = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
                     p.Start();
 
-                    Message = ProcessName + " executed.";
-                    return true;
+                    if (p.WaitForExit(ApplicationTimeoutSeconds * 1000))
+                    {
+                        if (p.ExitCode == 0)
+                        {
+                            Message = ProcessName + " executed.";
+                            string result = p.StandardOutput.ReadToEnd();
+                            AppendToMessage(result);
+
+                            if (!String.IsNullOrEmpty(FlowControlLabel))
+                                InstructionSet.InstructionSetContainer[FlowControlLabel] = result;
+
+                            break;
+                        }
+                        else
+                        {
+                            string result = p.StandardError.ReadToEnd();
+
+                            if (!String.IsNullOrEmpty(FlowControlLabel))
+                                InstructionSet.InstructionSetContainer[FlowControlLabel] = result;
+
+                            throw new Exception("Execution Error.\r\n" + result);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            p.Kill();
+                        }
+                        catch { }
+
+                        throw new Exception("Execution timed out.");
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -66,42 +118,11 @@ namespace STEM.Surge.Machine
                 }
             }
 
-            return false;
+            return Exceptions.Count == 0;
         }
 
         protected override void _Rollback()
         {
-            if (Stage == STEM.Surge.Stage.Ready)
-                return;
-
-            foreach (Process p in Process.GetProcessesByName(ProcessName))
-            {
-                int retry = 0;
-                while (retry <= Retry && !Stop)
-                {
-                    try
-                    {
-                        p.Kill();
-                        Message = ProcessName + " killed.";
-                        return;
-                    }
-                    catch (Exception ex)
-                    {
-                        if (retry >= Retry)
-                        {
-                            Exceptions.Add(ex);
-                            Message += ex.Message;
-                            return;
-                        }
-                        else
-                        {
-                            Thread.Sleep(TimeSpan.FromSeconds(RetryDelaySeconds));
-                        }
-                    }
-
-                    retry++;
-                }
-            }
         }
     }
 }
