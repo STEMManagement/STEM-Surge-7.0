aee758bd4c448a4bce365473258f7fbb6aa53f38
STEM Management <brendan.b.richards@gmail.com>
2020-02-21 09:27:11 +0000

    Changed the TextBox control to an RTB control

--- STEM.Surge/STEM.Surge.ControlPanel/StringCollectionEditor.Designer.cs
+++ STEM.Surge/STEM.Surge.ControlPanel/StringCollectionEditor.Designer.cs
@@ -29,29 +29,18 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StringCollectionEditor));
-            this.strings = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.apply = new System.Windows.Forms.Button();
             this.cancel = new System.Windows.Forms.Button();
+            this.strings = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // strings
-            // 
-            this.strings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.strings.Location = new System.Drawing.Point(4, 24);
-            this.strings.Multiline = true;
-            this.strings.Name = "strings";
-            this.strings.Size = new System.Drawing.Size(560, 222);
-            this.strings.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(4, 5);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.Size = new System.Drawing.Size(289, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "If configuring a collection of strings, enter one string per line.";
             // 
@@ -77,15 +66,26 @@
             this.cancel.UseVisualStyleBackColor = true;
             this.cancel.Click += new System.EventHandler(this.cancel_Click);
             // 
+            // strings
+            // 
+            this.strings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.strings.Location = new System.Drawing.Point(7, 21);
+            this.strings.Name = "strings";
+            this.strings.Size = new System.Drawing.Size(557, 225);
+            this.strings.TabIndex = 3;
+            this.strings.Text = "";
+            // 
             // StringCollectionEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(568, 281);
+            this.Controls.Add(this.strings);
             this.Controls.Add(this.cancel);
             this.Controls.Add(this.apply);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.strings);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StringCollectionEditor";
@@ -98,10 +98,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox strings;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button apply;
         private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.RichTextBox strings;
     }
 }
