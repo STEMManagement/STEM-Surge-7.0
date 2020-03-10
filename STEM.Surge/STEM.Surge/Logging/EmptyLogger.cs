4cb7d3c47f158eb1b473a7f7b87ea2c12e770f31
STEM Management <brendan.b.richards@gmail.com>
2020-03-10 06:15:09 +0000

    Still squeezing Logging out of Internal library

--- STEM.Surge/STEM.Surge/Logging/EmptyLogger.cs
+++ STEM.Surge/STEM.Surge/Logging/EmptyLogger.cs
@@ -4,25 +4,53 @@ using System.Text;
 
 namespace STEM.Surge.Logging
 {
+    public class EmptyLoggerReference : Exception
+    {
+        public EmptyLoggerReference() { }
+        public EmptyLoggerReference(string message) : base(message) { }
+    }
+
     public class EmptyLogger : ILogger
     {
-        public override Guid LogEvent(Guid objectID, string eventName, string processName, DateTime eventTime)
+        public override Guid LogEvent(Guid objectID, string eventName, string processName, DateTime eventTime, out List<Exception> exceptions)
         {
+            exceptions = new List<Exception>(new Exception[] { new EmptyLoggerReference("The EmptyLogger was called.") });
             return Guid.Empty;
         }
 
-        public override Guid LogEvent(string objectName, string eventName, string processName, DateTime eventTime)
+        public override Guid LogEvent(string objectName, string eventName, string processName, DateTime eventTime, out List<Exception> exceptions)
         {
+            exceptions = new List<Exception>(new Exception[] { new EmptyLoggerReference("The EmptyLogger was called.") });
             return Guid.Empty;
         }
 
-        public override bool LogEventMetadata(Guid eventID, string metadata)
+        public override bool LogEventMetadata(Guid eventID, string metadata, out List<Exception> exceptions)
         {
+            exceptions = new List<Exception>(new Exception[] { new EmptyLoggerReference("The EmptyLogger was called.") });
             return false;
         }
 
-        public override bool SetObjectInfo(Guid objectID, string objectName, DateTime creationTime)
+        public override bool SetObjectInfo(Guid objectID, string objectName, DateTime creationTime, out List<Exception> exceptions)
         {
+            exceptions = new List<Exception>(new Exception[] { new EmptyLoggerReference("The EmptyLogger was called.") });
+            return false;
+        }
+
+        public override bool BulkLoad(List<EventData> events, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>(new Exception[] { new EmptyLoggerReference("The EmptyLogger was called.") });
+            return false;
+        }
+
+        public override bool BulkLoad(List<EventMetadata> meta, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>(new Exception[] { new EmptyLoggerReference("The EmptyLogger was called.") });
+            return false;
+        }
+
+        public override bool BulkLoad(List<ObjectData> objects, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>(new Exception[] { new EmptyLoggerReference("The EmptyLogger was called.") });
             return false;
         }
     }
--- STEM.Surge/STEM.Surge/Logging/ILogger.cs
+++ STEM.Surge/STEM.Surge/Logging/ILogger.cs
@@ -25,13 +25,78 @@ namespace STEM.Surge.Logging
             return ret;
         }
 
+        public class EventData
+        {
+            public EventData() { }
+
+            public EventData(Guid objectID, string eventName, string processName, DateTime eventTime)
+            {
+                EventID = Guid.NewGuid();
+                ObjectID = objectID;
+                ObjectName = "";
+                EventName = eventName;
+                ProcessName = processName;
+                MachineName = STEM.Sys.IO.Net.MachineName();
+                EventTime = eventTime;
+            }
+
+            public EventData(string objectName, string eventName, string processName, DateTime eventTime)
+            {
+                EventID = Guid.NewGuid();
+                ObjectID = Guid.Empty;
+                ObjectName = objectName;
+                EventName = eventName;
+                ProcessName = processName;
+                MachineName = STEM.Sys.IO.Net.MachineName();
+                EventTime = eventTime;
+            }
+
+            public Guid EventID { get; set; }
+            public Guid ObjectID { get; set; }
+            public string ObjectName { get; set; }
+            public string EventName { get; set; }
+            public string ProcessName { get; set; }
+            public string MachineName { get; set; }
+            public DateTime EventTime { get; set; }
+        }
+
+        public class EventMetadata
+        {
+            public EventMetadata() { }
+
+            public EventMetadata(Guid eventID, string metadata)
+            {
+                EventID = eventID;
+                Metadata = metadata;
+            }
+
+            public Guid EventID { get; set; }
+            public string Metadata { get; set; }
+        }
+
+        public class ObjectData
+        {
+            public ObjectData() { }
+
+            public ObjectData(Guid id, string name, DateTime creationTime)
+            {
+                ID = id;
+                Name = name;
+                CreationTime = creationTime;
+            }
+
+            public Guid ID { get; set; }
+            public string Name { get; set; }
+            public DateTime CreationTime { get; set; }
+        }
+
         /// <summary>
         /// Log an event on an object
         /// </summary>
         /// <param name="eventID">The Guid of the event to which thismeta data belongs</param>
         /// <param name="meta">The metadata to record</param>
         /// <returns>The true on success.</returns>
-        public abstract bool LogEventMetadata(Guid eventID, string metadata);
+        public abstract bool LogEventMetadata(Guid eventID, string metadata, out List<Exception> exceptions);
 
         /// <summary>
         /// Log an event on an object
@@ -41,7 +106,7 @@ namespace STEM.Surge.Logging
         /// <param name="processName">The name of the proces logging the event</param>
         /// <param name="eventTime">The time of the event</param>
         /// <returns>The Guid of the Event recorded (Guid.Empty upon failure).</returns>
-        public abstract Guid LogEvent(Guid objectID, string eventName, string processName, DateTime eventTime);
+        public abstract Guid LogEvent(Guid objectID, string eventName, string processName, DateTime eventTime, out List<Exception> exceptions);
 
         /// <summary>
         /// Log an event on an object, defaulting eventTime to Utc Now
@@ -50,9 +115,9 @@ namespace STEM.Surge.Logging
         /// <param name="eventName">The name of the event</param>
         /// <param name="processName">The name of the proces logging the event</param>
         /// <returns>The Guid of the Event recorded (Guid.Empty upon failure).</returns>
-        public virtual Guid LogEvent(Guid objectID, string eventName, string processName)
+        public virtual Guid LogEvent(Guid objectID, string eventName, string processName, out List<Exception> exceptions)
         {
-            return LogEvent(objectID, eventName, processName, DateTime.UtcNow);
+            return LogEvent(objectID, eventName, processName, DateTime.UtcNow, out exceptions);
         }
 
         /// <summary>
@@ -63,7 +128,7 @@ namespace STEM.Surge.Logging
         /// <param name="processName">The name of the proces logging the event</param>
         /// <param name="eventTime">The time of the event</param>
         /// <returns>The Guid of the Event recorded (Guid.Empty upon failure).</returns>
-        public abstract Guid LogEvent(string objectName, string eventName, string processName, DateTime eventTime);
+        public abstract Guid LogEvent(string objectName, string eventName, string processName, DateTime eventTime, out List<Exception> exceptions);
 
         /// <summary>
         /// Log an event on an object, defaulting eventTime to Utc Now
@@ -72,9 +137,9 @@ namespace STEM.Surge.Logging
         /// <param name="eventName">The name of the event</param>
         /// <param name="processName">The name of the proces logging the event</param>
         /// <returns>The Guid of the Event recorded (Guid.Empty upon failure).</returns>
-        public virtual Guid LogEvent(string objectName, string eventName, string processName)
+        public virtual Guid LogEvent(string objectName, string eventName, string processName, out List<Exception> exceptions)
         {
-            return LogEvent(objectName, eventName, processName, DateTime.UtcNow);
+            return LogEvent(objectName, eventName, processName, DateTime.UtcNow, out exceptions);
         }
 
         /// <summary>
@@ -84,9 +149,9 @@ namespace STEM.Surge.Logging
         /// <param name="processName">The name of the proces logging the event</param>
         /// <param name="eventTime">The time of the event</param>
         /// <returns>The Guid of the Event recorded (Guid.Empty upon failure).</returns>
-        public virtual Guid LogEvent(string eventName, string processName, DateTime eventTime)
+        public virtual Guid LogEvent(string eventName, string processName, DateTime eventTime, out List<Exception> exceptions)
         {
-            return LogEvent(Guid.Empty, eventName, processName, eventTime);
+            return LogEvent(Guid.Empty, eventName, processName, eventTime, out exceptions);
         }
 
         /// <summary>
@@ -95,9 +160,9 @@ namespace STEM.Surge.Logging
         /// <param name="eventName">The name of the event</param>
         /// <param name="processName">The name of the proces logging the event</param>
         /// <returns>The Guid of the Event recorded (Guid.Empty upon failure).</returns>
-        public virtual Guid LogEvent(string eventName, string processName)
+        public virtual Guid LogEvent(string eventName, string processName, out List<Exception> exceptions)
         {
-            return LogEvent(Guid.Empty, eventName, processName, DateTime.UtcNow);
+            return LogEvent(Guid.Empty, eventName, processName, DateTime.UtcNow, out exceptions);
         }
 
         /// <summary>
@@ -107,7 +172,7 @@ namespace STEM.Surge.Logging
         /// <param name="objectName">The name of the object (e.g. filename sans path)</param>
         /// <param name="creationTime">The creation time of the object (e.g. file creation time UTC)</param>
         /// <returns>True if recorded</returns>
-        public abstract bool SetObjectInfo(Guid objectID, string objectName, DateTime creationTime);
+        public abstract bool SetObjectInfo(Guid objectID, string objectName, DateTime creationTime, out List<Exception> exceptions);
 
         /// <summary>
         /// Set/Update metadata about an object, defaulting creationTime to Utc Now
@@ -115,22 +180,45 @@ namespace STEM.Surge.Logging
         /// <param name="objectID">The Guid of the object about which the metadata applies</param>
         /// <param name="objectName">The name of the object (e.g. filename sans path)</param>
         /// <returns>True if recorded</returns>
-        public virtual bool SetObjectInfo(Guid objectID, string objectName)
+        public virtual bool SetObjectInfo(Guid objectID, string objectName, out List<Exception> exceptions)
         {
-            return SetObjectInfo(objectID, objectName, DateTime.UtcNow);
+            return SetObjectInfo(objectID, objectName, DateTime.UtcNow, out exceptions);
         }
-        
+
+        public abstract bool BulkLoad(List<EventData> events, out List<Exception> exceptions);
+        public abstract bool BulkLoad(List<ObjectData> objects, out List<Exception> exceptions);
+        public abstract bool BulkLoad(List<EventMetadata> meta, out List<Exception> exceptions);
+
         protected sealed override void _Rollback()
         {
         }
 
         protected sealed override bool _Run()
         {
-            // Plant logger
+            // Open logger
 
-            STEM.Sys.Global.Session[LoggerName] = this;
+            Open();
 
             return true;
+        }
+
+        protected virtual void Open()
+        {
+            lock (STEM.Sys.Global.Session)
+            {
+                if (STEM.Sys.Global.Session[LoggerName] != null && STEM.Sys.Global.Session[LoggerName] != this)
+                    ((ILogger)STEM.Sys.Global.Session[LoggerName]).Close();
+
+                STEM.Sys.Global.Session[LoggerName] = this;
+            }
+        }
+
+        protected virtual void Close()
+        {
+            lock (STEM.Sys.Global.Session)
+            {
+                STEM.Sys.Global.Session[LoggerName] = null;
+            }
         }
     }
 }
