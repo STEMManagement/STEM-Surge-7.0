d8c4afc8be7bcefa261f099bdba14038f61472f5
STEM Management <brendan.b.richards@gmail.com>
2020-03-10 09:04:35 +0000

    Switched to a Sys utility for IsWindows check

--- STEM.Surge/Extensions/STEM.Surge.SMB/SmbAuthenticate.cs
+++ STEM.Surge/Extensions/STEM.Surge.SMB/SmbAuthenticate.cs
@@ -79,7 +79,7 @@ namespace STEM.Surge.SMB
         {
             try
             {
-                bool isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows);
+                bool isWindows = STEM.Sys.Control.IsWindows;
 
                 if (!isWindows)
                     return true;
