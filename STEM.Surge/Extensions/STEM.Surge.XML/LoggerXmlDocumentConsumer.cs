d05da992d30a7306e5b1e84632b52c1d9a9b5265
STEM Management <brendan.b.richards@gmail.com>
2020-03-30 12:16:29 +0000

    Added an XmlLogger

---
+++ STEM.Surge/Extensions/STEM.Surge.XML/LoggerXmlDocumentConsumer.cs
@@ -0,0 +1,140 @@
+﻿/*
+ * Copyright 2019 STEM Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Linq;
+using System.Linq;
+using STEM.Surge.Logging;
+
+namespace STEM.Surge.XML
+{
+    [Serializable()]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    [DisplayName("Logger XmlDocument Consumer")]
+    [Description("Read in Xml Logger Documents and feed them into a different Logger.")]
+    public class LoggerXmlDocumentConsumer : Instruction
+    {
+        [Category("Logger")]
+        [DisplayName("Logger Name"), DescriptionAttribute("The logger into which the Xml Logs should be ingested.")]
+        public string LoggerName { get; set; }
+
+        [Category("Logger")]
+        [DisplayName("File List (Property Name: FileList)"), DescriptionAttribute("List<string> Property for the GroupingController to populate.")]
+        public List<string> FileList { get; set; }
+
+        protected override void _Rollback()
+        {
+        }
+
+        protected override bool _Run()
+        {
+            List<Exception> exceptions = new List<Exception>();
+
+            try
+            {
+                List<ILogger.EventData> events = new List<ILogger.EventData>();
+                List<ILogger.ObjectData> objects = new List<ILogger.ObjectData>();
+                List<ILogger.EventMetadata> meta = new List<ILogger.EventMetadata>();
+
+                List<string> filesConsumed = new List<string>();
+
+                foreach (string file in FileList)
+                {
+                    try
+                    {
+                        XDocument doc = XDocument.Load(new System.Xml.XmlTextReader(new System.IO.StringReader(System.IO.File.ReadAllText(STEM.Sys.IO.Path.AdjustPath(file)))));
+
+                        foreach (XElement e in doc.Descendants().Where(i => i.Name.LocalName == "ObjectData").ToList())
+                        {
+                            ILogger.ObjectData d = new ILogger.ObjectData
+                            {
+                                ID = Guid.Parse(e.Descendants().FirstOrDefault(i => i.Name.LocalName == "ID").Value),
+                                Name = e.Descendants().FirstOrDefault(i => i.Name.LocalName == "Name").Value,
+                                CreationTime = DateTime.Parse(e.Descendants().FirstOrDefault(i => i.Name.LocalName == "CreationTime").Value)
+                            };
+
+                            objects.Add(d);
+                        }
+
+                        foreach (XElement e in doc.Descendants().Where(i => i.Name.LocalName == "EventData").ToList())
+                        {
+                            ILogger.EventData d = new ILogger.EventData
+                            {
+                                EventID = Guid.Parse(e.Descendants().FirstOrDefault(i => i.Name.LocalName == "EventID").Value),
+                                ObjectID = Guid.Parse(e.Descendants().FirstOrDefault(i => i.Name.LocalName == "ObjectID").Value),
+                                ObjectName = e.Descendants().FirstOrDefault(i => i.Name.LocalName == "ObjectName").Value,
+                                EventName = e.Descendants().FirstOrDefault(i => i.Name.LocalName == "EventName").Value,
+                                ProcessName = e.Descendants().FirstOrDefault(i => i.Name.LocalName == "ProcessName").Value,
+                                MachineName = e.Descendants().FirstOrDefault(i => i.Name.LocalName == "MachineName").Value,
+                                EventTime = DateTime.Parse(e.Descendants().FirstOrDefault(i => i.Name.LocalName == "EventTime").Value)
+                            };
+
+                            events.Add(d);
+                        }
+
+                        foreach (XElement e in doc.Descendants().Where(i => i.Name.LocalName == "EventMetadata").ToList())
+                        {
+                            ILogger.EventMetadata d = new ILogger.EventMetadata
+                            {
+                                EventID = Guid.Parse(e.Descendants().FirstOrDefault(i => i.Name.LocalName == "EventID").Value),
+                                Metadata = e.Descendants().FirstOrDefault(i => i.Name.LocalName == "Metadata").Value
+                            };
+
+                            meta.Add(d);
+                        }
+
+                        filesConsumed.Add(STEM.Sys.IO.Path.AdjustPath(file));
+                    }
+                    catch { }
+                }
+
+                ILogger logger = STEM.Surge.Logging.ILogger.GetLogger(LoggerName);
+
+                if (objects.Count > 0)
+                    if (!logger.BulkLoad(objects, out exceptions))
+                        throw new Exception("Error loading Objects");
+
+                if (events.Count > 0)
+                    if (!logger.BulkLoad(events, out exceptions))
+                        throw new Exception("Error loading Events");
+
+                if (meta.Count > 0)
+                    if (!logger.BulkLoad(meta, out exceptions))
+                        throw new Exception("Error loading EventMetadata");
+
+                foreach (string file in filesConsumed)
+                {
+                    try
+                    {
+                        System.IO.File.Delete(file);
+                    }
+                    catch { }
+                }
+            }
+            catch (Exception ex)
+            {
+                Exceptions.Add(ex);
+                foreach (Exception e in exceptions)
+                    Exceptions.Add(e);
+            }
+
+            return Exceptions.Count() == 0;
+        }
+    }
+}
---
+++ STEM.Surge/Extensions/STEM.Surge.XML/XmlLogger.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Copyright 2019 STEM Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using STEM.Surge.Logging;
+
+namespace STEM.Surge.XML
+{
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    [DisplayName("XmlLogger")]
+    [Description("Plants a named ILogger in session that is backed by an Xml Document Store. " +
+        "This is used by the STEM.Surge.ObjectTracking extension or directly through ILogger.GetLogger(loggerName).")]
+    public class XmlLogger : ILogger
+    {
+        [Category("Logger")]
+        [DisplayName("Xml Document Store"), DescriptionAttribute("The directory where XmlDocuments will be written.")]
+        public string XmlDocumentStore { get; set; }
+
+        public XmlLogger()
+        {
+            XmlDocumentStore = @"D:\STEM.Workforce\XmlLogger";
+        }
+
+        bool WriteData(string data, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+            
+            XmlDocumentStore = STEM.Sys.IO.Path.AdjustPath(XmlDocumentStore);
+
+            try
+            {
+                if (!Directory.Exists(XmlDocumentStore))
+                    Directory.CreateDirectory(XmlDocumentStore);
+
+                File.WriteAllText(Path.Combine(XmlDocumentStore, Guid.NewGuid() + ".xml"), data);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
+
+            return false;
+        }
+
+        public override bool BulkLoad(List<EventData> events, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+            try
+            {
+                return WriteData(STEM.Sys.Serializable.Serialize(events), out exceptions);
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
+
+            return false;
+        }
+
+        public override bool BulkLoad(List<ObjectData> objects, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+            try
+            {
+                return WriteData(STEM.Sys.Serializable.Serialize(objects), out exceptions);
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
+
+            return false;
+        }
+
+        public override bool BulkLoad(List<EventMetadata> meta, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+            try
+            {
+                return WriteData(STEM.Sys.Serializable.Serialize(meta), out exceptions);
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
+
+            return false;
+        }
+
+        public override Guid LogEvent(Guid objectID, string eventName, string processName, DateTime eventTime, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+
+            Guid eventID = Guid.NewGuid();
+            bool success = BulkLoad(new List<EventData>(new EventData[]{ new EventData {
+                EventID = eventID,
+                ObjectID = objectID,
+                ObjectName = "",
+                EventName = eventName,
+                ProcessName = processName,
+                MachineName = STEM.Sys.IO.Net.MachineName(),
+                EventTime = eventTime } }), out exceptions);
+
+            if (success)
+                return eventID;
+
+            return Guid.Empty;
+        }
+
+        public override Guid LogEvent(string objectName, string eventName, string processName, DateTime eventTime, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+
+            Guid eventID = Guid.NewGuid();
+            bool success = BulkLoad(new List<EventData>(new EventData[]{ new EventData {
+                EventID = eventID,
+                ObjectID = Guid.Empty,
+                ObjectName = objectName,
+                EventName = eventName,
+                ProcessName = processName,
+                MachineName = STEM.Sys.IO.Net.MachineName(),
+                EventTime = eventTime } }), out exceptions);
+
+            if (success)
+                return eventID;
+
+            return Guid.Empty;
+        }
+
+        public override bool LogEventMetadata(Guid eventID, string metadata, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+
+            return BulkLoad(new List<EventMetadata>(new EventMetadata[] { new EventMetadata {
+                EventID = eventID,
+                Metadata = metadata } }), out exceptions);
+        }
+
+        public override bool SetObjectInfo(Guid objectID, string objectName, DateTime creationTime, out List<Exception> exceptions)
+        {
+            exceptions = new List<Exception>();
+
+            return BulkLoad(new List<ObjectData>(new ObjectData[] { new ObjectData {
+                ID = objectID,
+                Name = objectName,
+                CreationTime = creationTime } }), out exceptions);
+        }
+    }
+}
